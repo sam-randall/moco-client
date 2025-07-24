4105fca5bf6b36e2853d8214b746e25ea5bd8948
Sam Randall <sam.randall5@gmail.com>
2025-07-23 22:45:19 -0700

    add basic user facing ingredients

---
+++ main.py
@@ -0,0 +1,78 @@
+import numpy as np
+import requests
+import json
+import pandas as pd
+
+from pydantic import BaseModel
+from typing import Any, List, Optional
+class RulesRequest(BaseModel):
+    data: List
+    predictions: List
+
+class Rule(BaseModel):
+    coef: List
+    intercept: List
+    threshold: float
+    quad_coef: Optional[List] = None
+
+class RulesResponse(BaseModel):
+    rules: List[Rule]
+    rule_values: List[Any]
+    rule_information: List
+
+
+# TODO: Move to .env
+IS_DEV = True
+URL = f'{"127.0.0.1" if IS_DEV else "production-url"}'
+
+class EarlyExitModel:
+    def __init__(self, model):
+        self.model = model
+        self.membership_rules = []
+        self.membership_values = []
+
+    def get_fast_rules(self, dataset: np.ndarray, predictions: np.ndarray):
+        dataset = dataset.tolist()
+        predictions = predictions.tolist()
+        d = {
+            'data': dataset,
+            'predictions': predictions
+        }
+
+        r = requests.post("http://127.0.0.1:8000/get-rules", json = d)
+
+        if r.status_code == 200:
+            d = json.loads(r.content)
+
+            rules = d['rules']
+            rule_values = d['rule_values']
+            rule_summary = d.get('rule_summary', None)
+
+            out = [Rule.model_validate(r) for r in rules]
+            self.membership_rules = out
+            self.membership_values = rule_values
+            if rule_summary is not None:
+                out = pd.DataFrame(rule_summary)
+                return out
+        else:
+            return None
+
+    def predict(self, x: np.ndarray):
+        pass
+
+def main():
+    m = EarlyExitModel(None)
+    data1 = np.random.uniform(low = 0, high = 0.1, size = (20, 2))
+    data2 = np.random.uniform(low = 1, high = 1.1, size = (20, 2))
+    stack = np.vstack([data1, data2])
+
+    predictions = np.zeros(40)
+    predictions[20:40] = 1
+    m.get_fast_rules(stack, predictions)
+    print(m.membership_rules)
+    print(m.membership_values)
+
+
+if __name__ == "__main__":
+    main()
+
