3c1c6d655536f604841d2ebe8ded653c09cf6bd5
Sam Randall <sam.randall5@gmail.com>
2025-07-24 20:10:47 -0700

    add pyproject

---
+++ feature.py
@@ -0,0 +1,19 @@
+from moco.coloring import compute_categorical_coloring, compute_numerical_coloring
+from typing import List
+import numpy as np
+
+
+class Feature:
+    def __init__(self, values: np.ndarray, is_categorical: bool):
+        self.values = values
+        self.is_categorical = is_categorical
+
+    def as_coloring(self, to_hex: bool = True) -> List:
+        return (
+            compute_categorical_coloring(self.values)
+            if self.is_categorical
+            else compute_numerical_coloring(self.values, to_hex=to_hex)
+        )
+
+    def to_dict(self):
+        return {"values": self.values.tolist(), "is_categorical": self.is_categorical}
