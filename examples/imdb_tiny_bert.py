e329204a1d8662c9cfe9c420943a7b02673951cc
Sam Randall <sam.randall5@gmail.com>
2025-07-24 20:19:35 -0700

    Up and running

---
+++ examples/imdb_tiny_bert.py
@@ -0,0 +1,27 @@
+
+
+from datasets import load_dataset
+from transformers import pipeline
+from integrations.torch.bert_classification_model import EarlyExitTextClassificationModel
+from src.ml_frame import MLFrame
+
+def main():
+    ds = load_dataset("stanfordnlp/imdb")
+    train_ds = ds['train']
+    test_ds = ds['test']
+
+    tiny_bert = pipeline("text-classification", "arnabdhar/tinybert-imdb")
+
+    tiny_bert.model.to('cpu')
+    print("Loaded Tiny BERT")
+
+
+    eetcm = EarlyExitTextClassificationModel(tiny_bert.model)
+
+    frame = MLFrame.load_frame('../moco/imdb/out.frame')
+    embedding = frame['bert.encoder.layer.0.attention']
+    N = embedding.shape[0]
+
+
+if __name__ == "__main__":
+    main()
