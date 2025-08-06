446a899466546e3abd64f27dbf09c10038811d93
Sam Randall <sam.randall5@gmail.com>
2025-08-05 23:20:24 -0700

    Improve profiling -- remove cold start problem

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from early_exit_model import EarlyExitModel
 
 def main():
     X, y = load_breast_cancer(return_X_y=True)
-    mlp = MLPClassifier()
+    mlp = MLPClassifier(hidden_layer_sizes=16)
     mlp.fit(X, y)
 
     m = EarlyExitModel(mlp)
@@ -19,15 +19,19 @@ def main():
     summary = m.get_fast_rules('iris.npy', 'iris_predictions.npy')
     print(summary)
 
+    for i in range(5):
+        _ = m.predict(X)
     start = time.time()
-    new_predictions = m.predict(X)
+    for i in range(5):
+        new_predictions = m.predict(X)
     end = time.time()
-    print(end - start)
+    print("Experimental", end - start)
 
     start = time.time()
-    out = mlp.predict(X)
+    for i in range(5):
+        out = mlp.predict(X)
     end = time.time()
-    print(end - start)
+    print("Original", end - start)
     adherence = (new_predictions == out).mean()
     print("Adherence:", adherence)
 
